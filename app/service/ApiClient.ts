5c5285940134f8b51ece3119b3d84f73ed080aa1
luando2208 <dodangminhluan@gmail.com>
2025-08-22 22:13:32 +0700

    hi

--- app/service/ApiClient.ts
+++ app/service/ApiClient.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 export const axiosClient = axios.create({
-  baseURL: "http://localhost:8080/api/",
+  baseURL: "https://ourhope.io.vn/api",
   timeout: 30000,
   headers: {
     "Content-Type": "application/json",
