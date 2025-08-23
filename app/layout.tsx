dcc2fde0c1ecd49579881b53cb4962d51477f479
luando2208 <dodangminhluan@gmail.com>
2025-08-23 18:33:44 +0700

    deploy 23/8

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,6 +41,7 @@ export default async function RootLayout({
                   style={{ zIndex: 9999 }}
                 />
                 <HamburgerMenu />
+                {/* he */}
 
                 <>
                   <CreatePost token={token || ""} />
